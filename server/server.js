c187e4b8f05903ba8e61ec3ce9c1c2989c5c2edc
idbdyoung <idbd.young@gmail.com>
2023-01-11 21:33:18 +0900

    이미지 업로드 기능 추가 - 새로운 이미지 이름 생성 - uploads 폴더에 저장

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const multer = require("multer");
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) =>
+    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+});
+const upload = multer({ storage });
 
 const app = express();
 const PORT = 3003;
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
   res.json({ result: "ok" });
 });
 
